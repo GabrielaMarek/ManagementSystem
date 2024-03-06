a68af44fe330acb5026e01eb7098734f2a17b4f8
Gabriela Marek <w67497@student.wsiz.edu.pl>
2024-03-07 00:06:04 +0100

    Login form layout

--- ManagementSystem/Form1.Designer.cs
+++ ManagementSystem/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace ManagementSystem
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ManagementSystem
 {
     partial class Form1
     {
@@ -29,25 +32,79 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.placeHolderTextBox2 = new ManagementSystem.PlaceHolderTextBox();
+            this.placeHolderTextBox1 = new ManagementSystem.PlaceHolderTextBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(72)))), ((int)(((byte)(165)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(112)))), ((int)(((byte)(255)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(92)))), ((int)(((byte)(235)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(1235, 615);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(520, 143);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // placeHolderTextBox2
+            // 
+            this.placeHolderTextBox2.BackColor = System.Drawing.Color.White;
+            this.placeHolderTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.placeHolderTextBox2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Italic);
+            this.placeHolderTextBox2.ForeColor = System.Drawing.Color.Gray;
+            this.placeHolderTextBox2.Location = new System.Drawing.Point(1177, 440);
+            this.placeHolderTextBox2.Multiline = true;
+            this.placeHolderTextBox2.Name = "placeHolderTextBox2";
+            this.placeHolderTextBox2.PlaceHolderText = "Password";
+            this.placeHolderTextBox2.Size = new System.Drawing.Size(646, 107);
+            this.placeHolderTextBox2.TabIndex = 1;
+            this.placeHolderTextBox2.Text = "Password";
+            // 
+            // placeHolderTextBox1
+            // 
+            this.placeHolderTextBox1.BackColor = System.Drawing.Color.White;
+            this.placeHolderTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.placeHolderTextBox1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Italic);
+            this.placeHolderTextBox1.ForeColor = System.Drawing.Color.Gray;
+            this.placeHolderTextBox1.Location = new System.Drawing.Point(1176, 279);
+            this.placeHolderTextBox1.Multiline = true;
+            this.placeHolderTextBox1.Name = "placeHolderTextBox1";
+            this.placeHolderTextBox1.PlaceHolderText = "Username";
+            this.placeHolderTextBox1.Size = new System.Drawing.Size(646, 107);
+            this.placeHolderTextBox1.TabIndex = 0;
+            this.placeHolderTextBox1.Text = "Username";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(243)))), ((int)(((byte)(244)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1888, 992);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.placeHolderTextBox2);
+            this.Controls.Add(this.placeHolderTextBox1);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private PlaceHolderTextBox placeHolderTextBox1;
+        private PlaceHolderTextBox placeHolderTextBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ManagementSystem/Form1.cs
+++ ManagementSystem/Form1.cs
@@ -1,6 +1,12 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using System;
 
 namespace ManagementSystem
 {
@@ -9,8 +15,66 @@ namespace ManagementSystem
         public Form1()
         {
             InitializeComponent();
+        }
+    }
 
+    public class PlaceHolderTextBox : TextBox
+    {
+
+        bool isPlaceHolder = true;
+        string _placeHolderText;
+        public string PlaceHolderText
+        {
+            get { return _placeHolderText; }
+            set
+            {
+                _placeHolderText = value;
+                setPlaceholder();
+            }
         }
 
+        public new string Text
+        {
+            get => isPlaceHolder ? string.Empty : base.Text;
+            set => base.Text = value;
+        }
+
+        private void setPlaceholder()
+        {
+            if (string.IsNullOrEmpty(base.Text))
+            {
+                base.Text = PlaceHolderText;
+                this.ForeColor = Color.Gray;
+                this.Font = new Font(this.Font, FontStyle.Italic);
+                isPlaceHolder = true;
+            }
+        }
+
+        private void removePlaceHolder()
+        {
+
+            if (isPlaceHolder)
+            {
+                base.Text = "";
+                this.ForeColor = System.Drawing.SystemColors.WindowText;
+                this.Font = new Font(this.Font, FontStyle.Regular);
+                isPlaceHolder = false;
+            }
+        }
+        public PlaceHolderTextBox()
+        {
+            GotFocus += removePlaceHolder;
+            LostFocus += setPlaceholder;
+        }
+
+        private void setPlaceholder(object sender, EventArgs e)
+        {
+            setPlaceholder();
+        }
+
+        private void removePlaceHolder(object sender, EventArgs e)
+        {
+            removePlaceHolder();
+        }
     }
 }
